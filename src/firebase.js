20c1b4ee13d4ff9037b41d3e4080e879797bf405
50vincents <vincethai035@gmail.com>
2020-09-10 13:30:54 -0700

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDwctN0KROlwBXTTrUZlDo_2h0lMYt0dws",
+  authDomain: "clone-18e27.firebaseapp.com",
+  databaseURL: "https://clone-18e27.firebaseio.com",
+  projectId: "clone-18e27",
+  storageBucket: "clone-18e27.appspot.com",
+  messagingSenderId: "719668890295",
+  appId: "1:719668890295:web:62bc3b77999e97cc931b32",
+  measurementId: "G-RXEMPEM5W9"
+};
