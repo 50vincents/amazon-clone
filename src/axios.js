cc667b0567f4c71c0ae1c7e0c1bd9b8801954c42
Vincent Thai <thai.vincent@tcs.com>
2021-03-23 14:46:08 -0400

    test

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 // Fetching library, interact with api
 import axios from 'axios';
 
+// where backend is hosted
 const instance = axios.create({
   baseURL: 'https://us-central1-clone-18e27.cloudfunctions.net/api'
   // 'http://localhost:5001/clone-18e27/us-central1/api' // API URL (cloud function)
